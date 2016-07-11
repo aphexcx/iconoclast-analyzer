c8ad74a2e0d73589c05f050823fdc1a7c786336f
Afik Cohen <aphex@aphex.cx>
2016-07-10 22:46:15 -0700

    Remove EstimatedAge from Ad.

--- src/main/scala/Ad.scala
+++ src/main/scala/Ad.scala
@@ -1,10 +1,10 @@
 import spray.json.DefaultJsonProtocol
 
-case class Ad(_id: MongoObjectId, url: String, imageUrls: List[String], age: Int, title: String, text: String, estimatedAge: Double)
+case class Ad(_id: MongoObjectId, url: String, imageUrls: List[String], age: Int, title: String, text: String)
 
 case class MongoObjectId($oid: String)
 
 object AdJsonProtocol extends DefaultJsonProtocol {
   implicit val mongoOidFormat = jsonFormat1(MongoObjectId)
-  implicit val adFormat = jsonFormat7(Ad)
+  implicit val adFormat = jsonFormat6(Ad)
 }
